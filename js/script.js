f6872b4e610bf489acfe66c242589ef26a51e41d
yarovik1982 <65862979+yarovik1982@users.noreply.github.com>
2020-09-14 00:46:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('click', function(event) {
+    let id = event.target.dataset.toggleId;
+    if (!id) return;
+
+    let elem = document.getElementById(id);
+
+    elem.hidden = !elem.hidden;
+  });
